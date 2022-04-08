ffb040568ab852af67f8372ce2f2efc80b86fdfc
KONTUR\gilimkhanov <gilimkhanov@skbkontur.ru>
2022-04-08 12:05:06 +0545

    fix ?

--- compile_docs.bat
+++ compile_docs.bat
@@ -1,4 +1,4 @@
 chcp 65001
 cls
-python -m sphinx "./documentation RST/source" "../compiled" -a -E -c "./documentation RST/conf"
+python -m sphinx "./documentation RST/source" "../compiled" -a -E -c "./documentation RST/conf" -T
 @ > "../compiled/log.txt"
--- documentation RST/conf/conf.py
+++ documentation RST/conf/conf.py
@@ -3,6 +3,7 @@
 
 from sys import path as PATH_variable
 from os import path
+import pathlib as pl
 
 html_static_path = ['_static']
 html_style = 'css/diadoc 1C COM style.css'
@@ -16,7 +17,13 @@ extensions = [
 primary_domain = 'com-object'
 source_suffix = '.rst'
 exclude_patterns = []
-master_doc = '../source/index'
+
+root_path = pl.Path(__file__).parents[1]
+source_path = root_path / 'source'
+print('source_path: ', source_path)
+master_doc_path = source_path / 'index'
+print('master_path: ', master_doc_path)
+master_doc = str(master_doc_path)
 
 project = u'1C Addin/COM Диадок API'
 copyright = u'2022, Diadoc'
